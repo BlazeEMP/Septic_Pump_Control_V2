58db24ec2eadcd1055605fc61879dc7354264d27
BlazeEMP <blazelitanui@gmail.com>
2025-05-05 22:39:01 -0500

    added a tracking value that reads when the sensor shows a distance further than possible, if this happens from bubbles doing laundry, it will now run a shorter pump cycle to attempt to clear the bubbles

--- Sensor.ino
+++ Sensor.ino
@@ -3,107 +3,133 @@
 int sensorError = 0;  // tracking variable for errors in sensor, keep global to adjust with one function and read the value for comparison in another
 
 int sensorData() {
-  int loops = 6;                    // set number of loops for avg measurement
-  int rollingInput[loops] = { 0 };  // establish array to store sensor inputs while sorting through data
-  int distanceModifier = 10;             // sets a value to compare the modulus in data sorting later, prevents errant inputs from being considered, does not add to an error count like 0s
+	int loops = 6;                    // set number of loops for avg measurement
+	int rollingInput[loops] = { 0 };  // establish array to store sensor inputs while sorting through data
+	// distance modifier is not a great indicator when bubbles can form in the pit, ultrasonic sensor inputs should have wide acceptable range
+	int distanceModifier = 100;   // sets a value to compare the measurements in data sorting later, prevents errant inputs from being considered, does not add to an error count like 0s
 
-  // active error tracking
-  int zeroCount = 0;
-  int faultMax = 10;  // number of loops with more than zeroMax 0 readings before throwing error
+	// active error tracking
+	int zeroCount = 0;
+	int faultMax = 180;  // number of loops with more than zeroMax 0 readings before throwing error
 
-  for (int i = 0; i < loops; i++) {
-    int distance = pulseSensor();
+	for (int i = 0; i < loops; i++) {
+		int distance = pulseSensor();
 
-    if (distance != 0) {
-      if (avgDistance == 0 && i < 3) {  // this condition only runs if no avgDistance has not been set yet (besides initial 0) allows the establishment of a baseline on startup for error correction
-        rollingInput[i] = distance;
-        Serial.print("Resetting error correction input, avgDistance was set to 0");Serial.print("\t");Serial.print("rollingInput at index [");Serial.print(i);Serial.print("] = ");Serial.println(rollingInput[i]);
-      } else if (i < 2) {  // for first two spots in each loop check data against last measured and corrected avg measurement of the loop that was stored globally
-        // [**1] if data is considered bad, remove 1 from index tracker as we do with a 0 value input, disregard the gathered data and continue on to next for() iteration
-        // we want to make to make sure a bad input (decided by distanceModifier) isn't detected and stored in the array of distances, so anything greater than the int distanceModifier, as distance in cm, from the last avgDistance will be disregarded
-        if (distance < avgDistance && avgDistance - distance > distanceModifier) {
-          Serial.println("distance < avgDistance && avgDistance - distance > distanceModifier");
-          i--;
-          watchdog.reset();
-        } else if (distance > avgDistance && distance - avgDistance > distanceModifier) {
-          Serial.println("distance > avgDistance && distance - avgDistance > distanceModifier");
-          i--;
-          watchdog.reset();
-        } else {
-          rollingInput[i] = distance;
-          Serial.print("rollingInput at index [");Serial.print(i);Serial.print("] = ");Serial.println(rollingInput[i]);
-        }
-      } else { // all values past the first 3 (for first run of loop), and 2 (for all other loop cycles) values of the array are checked here using the same distanceModifier logic
-        int rollAvg = rollingInput[i - 1] + rollingInput[i - 2];  // two accepted inputs with error correction based on average (or being the only first 3 inputs)
-        rollAvg = rollAvg / 2;
-        // [**1]
-        if (distance < rollAvg && rollAvg - distance > distanceModifier) {
-          Serial.println("distance < rollAvg && rollAvg - distance > distanceModifier");
-          i--;
-          watchdog.reset();
-        } else if (distance > rollAvg && distance - rollAvg > distanceModifier) {
-          Serial.println("distance > rollAvg && distance - rollAvg > distanceModifier");
-          i--;
-          watchdog.reset();
-        } else {
-          rollingInput[i] = distance;
-          Serial.print("rollingInput at index [");Serial.print(i);Serial.print("] = ");Serial.println(rollingInput[i]);
-        }
+		if (distance != 0) {
+      if (distance > highReadingWarning) {
+        highReadingTracker ++;
       }
-      if (i == loops - 1 && zeroCount == 0) { // if we make it through a whole for loop reading no 0s we can reset the alarm trigger for the sensor
-        sensorError = 0;
-        checkSensor = false;
-      }
-    } else {
-      zeroCount++;
-      Serial.println(" 0! ");Serial.print("Zero count = ");Serial.println(zeroCount);
-      bool maxZeroReached = errorCheck(zeroCount);
-      i--;
-      if (maxZeroReached) {
-        // add a return for sensor error loops and boolean change if alarm conditions met
-        if (sensorError > faultMax) {  // if the count of loops with zeroCount being too high is greater  than the variable faultMax, set checkSensor error flag to true
-          Serial.println("faultMax reached");
-          checkSensor = true;
-        }
-        return 0; // since we aren't storing 0s in the array and allowing the index to progress by 1, once we reach the maximum number of 0s permitted by the set variable zeroMax in errorCheck() we can return 0 directly to save time on the averageOfLoops() calculation
-      }
-    }
+			if (avgDistance == 0 && i < 3) {  // this condition only runs if no avgDistance has been set yet (besides initial 0) allows the establishment of a baseline on startup
+				rollingInput[i] = distance;
+				Serial.print("Resetting error correction input, avgDistance was set to 0");
+				Serial.print("\t");
+				Serial.print("rollingInput at index [");
+				Serial.print(i);
+				Serial.print("] = ");
+				Serial.println(rollingInput[i]);
+			} else if (i < 2) {  // for first two spots in each loop check data against last measured and corrected avg measurement of the loop that was stored globally
+				// [**1] if data is considered bad, remove 1 from index tracker as we do with a 0 value input, disregard the gathered data and continue on to next for() iteration
+				// we want to make to make sure a bad input (decided by distanceModifier) isn't detected and stored in the array of distances, so anything greater than the int distanceModifier, as distance in cm, from the last avgDistance will be disregarded
+				if (distance < avgDistance && avgDistance - distance > distanceModifier) {
+					Serial.println("distance < avgDistance && avgDistance - distance > distanceModifier");
+					i--;
+					watchdog.reset();
+				} else if (distance > avgDistance && distance - avgDistance > distanceModifier) {
+					Serial.println("distance > avgDistance && distance - avgDistance > distanceModifier");
+					i--;
+					watchdog.reset();
+				} else {
+					rollingInput[i] = distance;
+					Serial.print("rollingInput at index [");
+					Serial.print(i);
+					Serial.print("] = ");
+					Serial.println(rollingInput[i]);
+				}
+			} else {                                                    // all values past the first 3 (for first run of loop), and 2 (for all other loop cycles) values of the array are checked here using the same distanceModifier logic
+				int rollAvg = rollingInput[i - 1] + rollingInput[i - 2];  // two accepted inputs with error correction based on average (or being the only first 3 inputs)
+				rollAvg = rollAvg / 2;
+				// [**1]
+				if (distance < rollAvg && rollAvg - distance > distanceModifier) {
+					Serial.println("distance < rollAvg && rollAvg - distance > distanceModifier");
+					i--;
+					watchdog.reset();
+				} else if (distance > rollAvg && distance - rollAvg > distanceModifier) {
+					Serial.println("distance > rollAvg && distance - rollAvg > distanceModifier");
+					i--;
+					watchdog.reset();
+				} else {
+					rollingInput[i] = distance;
+					Serial.print("rollingInput at index [");
+					Serial.print(i);
+					Serial.print("] = ");
+					Serial.println(rollingInput[i]);
+				}
+			}
+			if (i == loops - 1 && zeroCount == 0) {  // if we make it through a whole for loop reading no 0s we can reset the alarm trigger for the sensor
+				sensorError = 0;
+				checkSensor = false;
+			}
+		} else {
+			zeroCount++;
+			Serial.println(" 0! ");
+			Serial.print("Zero count = ");
+			Serial.println(zeroCount);
+			bool maxZeroReached = errorCheck(zeroCount);
+			i--;
+			if (maxZeroReached) {
+				// add a return for sensor error loops and boolean change if alarm conditions met
+				if (sensorError > faultMax) {  // if the count of loops with zeroCount being too high is greater than the variable faultMax, set checkSensor error flag to true
+					Serial.println("faultMax reached");
+					checkSensor = true;
+				}
+				return 0;  // since we aren't storing 0s in the array and allowing the index to progress by 1, once we reach the maximum number of 0s permitted by the set variable zeroMax in errorCheck() we can return 0 directly to save time on the averageOfLoops() calculation
+			}
+		}
+	}
+  if (highReadingTracker > highReadingLimit) {
+    autoRunPump(2500);
+    highReadingTracker = 0;
   }
-  return averageOfLoops(loops, rollingInput);
+	return averageOfLoops(loops, rollingInput);
 }
 
 int averageOfLoops(int loops, int rollingInput[]) {
-  int loopAvg = 0;  // used as return value for function, calculate avg of rollingInput at end of function, otherwise allow to return 0
-  for (int i = 0; i < loops; i++) {
-    loopAvg += rollingInput[i];
-  }
-  loopAvg = loopAvg / loops;
-  Serial.print("\n------Leaving sensor loop------\n");Serial.print("loopAvg = ");Serial.println(loopAvg);Serial.println("\n\n");
-  return loopAvg;
+	int loopAvg = 0;  // used as return value for function, calculate avg of rollingInput at end of function, otherwise should allow to return 0
+	for (int i = 0; i < loops; i++) {
+		loopAvg += rollingInput[i];
+	}
+	loopAvg = loopAvg / loops;
+	Serial.print("\n------Leaving sensor loop------\n");
+	Serial.print("loopAvg = ");
+	Serial.println(loopAvg);
+	Serial.println("\n\n");
+	return loopAvg;
 }
 
 int pulseSensor() {
-  digitalWrite(trig, HIGH);  // setting trigger pin high for 10-15 microseconds (not flexible, only try up to 20 micro)
-  delayMicroseconds(15);
-  digitalWrite(trig, LOW);                    // keep low until new trigger
-  int distance = pulseIn(echo, HIGH, 26000);  // reading ultrasonic reflections from modules output echo pin
+	digitalWrite(trig, HIGH);  // setting trigger pin high for 10-15 microseconds (not flexible, only try up to 20 micro)
+	delayMicroseconds(15);
+	digitalWrite(trig, LOW);                    // keep low until new trigger
+	int distance = pulseIn(echo, HIGH, 26000);  // reading ultrasonic reflections from modules output echo pin
 
-  // algorithm invloving speed of sound in air converted to centimeters or other
-  distance = (distance / 2) / 29.1;  // distance/58 for centimeters OR distance/ for inches (change displayed characters for units)
+	// algorithm invloving speed of sound in air converted to centimeters or other
+	distance = (distance / 2) / 29.1;  // distance/58 for centimeters OR distance/ for inches (change displayed characters for units)
 
-  delay(58);  //sets delay for sensing times (recommended 60ms total measurement cycle) we have spare time spent on calculations so can be reduced slightly
+	delay(58);  // sets delay for sensing times (recommended 60ms total measurement cycle) we have spare time spent on calculations so can be reduced slightly
 
-  Serial.print("Distance = ");Serial.print(distance);Serial.print("\t");
-  return distance;
+	Serial.print("Distance = ");
+	Serial.print(distance);
+	Serial.print("\t");
+	return distance;
 }
 
 // this function only sets the condition to true for checking sensor errors. We reset the alarm trigger by completeing a full for loop in sensorData() reading NO zeros at all
 bool errorCheck(int zeroCount) {
-  int zeroMax = 10;  // total number of 0s that can be in one loop before fault condition is met
+	int zeroMax = 10;  // total number of 0s that can be in one loop before fault condition is met
 
-  if (zeroCount >= zeroMax) {
-    sensorError++;
-    return true;
-  }
-  return false;
+	if (zeroCount >= zeroMax) {
+		sensorError++;
+		return true;
+	}
+	return false;
 }
--- Septic_Pump_Control_V2.ino
+++ Septic_Pump_Control_V2.ino
@@ -8,14 +8,14 @@
 Watchdog watchdog;  // bring watchdog class object in
 
 // section good for cross platform work when using print and write functions or want them to be changed
-//#if defined(ARDUINO) && ARDUINO >= 100 this if is enabled for Arduino UNO
-//#define printByte(args)  write(args); // keep if wanting to use lcd.printByte instead of lcd.write
-//#else
-//#define printByte(args)  print(args,BYTE);
-//#endif
+// #if defined(ARDUINO) && ARDUINO >= 100 this if is enabled for Arduino UNO
+// #define printByte(args)  write(args); // keep if wanting to use lcd.printByte instead of lcd.write
+// #else
+// #define printByte(args)  print(args,BYTE);
+// #endif
 
-#define upDistance 3         // raise the number of cm to trigger (trigger with lower water level)
-#define downDistance 4       // lower the number of cm to trigger (trigger with higher water level)
+#define upDistance 3      // raise the number of cm to trigger (trigger with lower water level)
+#define downDistance 4    // lower the number of cm to trigger (trigger with higher water level)
 #define echo 7            // input signal from ultrasonic sensor
 #define trig 8            // output pulses for ultrasonic sensor
 #define alarm 11          // pin for alarm to sound (go to 555 flip flop configuration for TRIGGER pin)
@@ -25,8 +25,13 @@ Watchdog watchdog;  // bring watchdog class object in
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-int avgDistance = 0;     // set average distance to global for use in multiple functions
-int heightSetting = 25;  // set default trigger height for water level, keep global to adjust with one function (adjustLevel) and read the value for comparison in another (setupDisplay, refreshDisplay, and autoRunPump)
+int avgDistance = 0;          // set average distance to global for use in multiple functions
+int heightSetting = 25;       // set default trigger height for water level, keep global to adjust with one function (adjustLevel) and read the value for comparison in another (setupDisplay, refreshDisplay, and autoRunPump)
+
+int highReadingWarning = 40;  // if the sensor reads over 32cm it is getting reflections from bubbles that slow down the speed of sound, if this lasts too long (track while looping through sensor readings) the alarm should sound
+int highReadingTracker = 0;
+int highReadingLimit = 400;
+int goodReadingTracker = 0;
 
 // alarm will also set off if arduino resets, also triggering relay reset for modules, does not turn off alarm when no longer true, manual button to reset latch must be pushed and all bools false
 bool checkSensor = false;  // fails on reporting too many zeros
@@ -34,41 +39,42 @@ bool checkRelay = false;   // current draw sense required
 bool checkWater = false;   // water sensor input required
 
 void setup() {
-  // enable watchdog with timer variable in seconds at the end, 1,2, or 4 works well for the application. If water levels can change quickly in the system consider evaluating all functions for new operating speed (less loops, more optimization) and watchdog reset positions
-  watchdog.enable(Watchdog::TIMEOUT_4S);
+	// enable watchdog with timer variable in seconds at the end, 1,2, or 4 works well for the application. If water levels can change quickly in the system consider evaluating all functions for new operating speed (less loops, more optimization) and watchdog reset positions
+	watchdog.enable(Watchdog::TIMEOUT_4S);
 
-  pinMode(watchdogRelay, OUTPUT);     // initialize "watchdog" pin to high
-  digitalWrite(watchdogRelay, HIGH);  // do not overwrite unless using custom code to reset on program hangups for power relay on board, may be set low in this project to reset the power manually but right now is tracking power-on state of arduino, watchdog handles the reset itself
+	pinMode(watchdogRelay, OUTPUT);     // initialize "watchdog" pin to high
+	digitalWrite(watchdogRelay, HIGH);  // do not overwrite unless using custom code to reset on program hangups for power relay on board, may be set low in this project to reset the power manually but right now is tracking power-on state of arduino, watchdog handles the reset itself
 
-  pinMode(alarm, OUTPUT);  // alarm trigger signal initialization, routes to NOT gate and 555 as a latch circuit
-  digitalWrite(alarm, LOW);
+	pinMode(alarm, OUTPUT);  // alarm trigger signal initialization, routes to NOT gate and 555 as a latch circuit
+	digitalWrite(alarm, LOW);
 
-  pinMode(runPumpRelay, OUTPUT);  // pin for relay to run pump
+	pinMode(runPumpRelay, OUTPUT);  // pin for relay to run pump
 
-  // pins for adjusting distance to trigger relay
-  pinMode(upDistance, INPUT_PULLUP);
-  pinMode(downDistance, INPUT_PULLUP);
+	// pins for adjusting distance to trigger relay
+	pinMode(upDistance, INPUT_PULLUP);
+	pinMode(downDistance, INPUT_PULLUP);
 
-  // pins for ultrasonic sensor
-  pinMode(echo, INPUT);
-  pinMode(trig, OUTPUT);
-  digitalWrite(trig, LOW);  // default low for ultrasonic sensor trigger pin
-  //digitalWrite(echo, HIGH);  // this could be intentional to set a pullup resistor, check functionality with sensor plugged in
-  setupDisplay();
-  Serial.begin(19200);
-  watchdog.reset();
-  Serial.println("\n\n\nRESTART COMPLETE ENTERING VOID LOOP()\n");
+	// pins for ultrasonic sensor
+	pinMode(echo, INPUT);
+	pinMode(trig, OUTPUT);
+	digitalWrite(trig, LOW);  // default low for ultrasonic sensor trigger pin
+	// digitalWrite(echo, HIGH);  // this could be intentional to set a pullup resistor, check functionality with sensor plugged in
+	setupDisplay();
+	Serial.begin(19200);
+	watchdog.reset();
+	Serial.println("\n\n\nRESTART COMPLETE ENTERING VOID LOOP()\n");
 }
 
 void loop() {
-  delay(800);
-  adjustLevel();
-  Serial.print(avgDistance);Serial.println(" is the average distance before sensorData()");
-  avgDistance = sensorData();
-  refreshDisplay();                               // useful if debugging sensorData since it loops back around very quickly when not running the pump
-  enableAlarm(checkSensor, checkRelay, checkWater);  // checks all three global booleans that are modified by above functions and checks based on external sensor input only
-  watchdog.reset();
-  autoRunPump(8000);  // pass in the length of time you want to run pump in milliseconds, auto refers to self check in function for conditions
-  // IMPORTANT INTERGER FOR PUMP TIME, NOT CURRENTLY VARIABLE WHILE INSTALLED, TIME SHOULD EQUAL NO MORE THAN THE TIME TO PUMP FROM MAX OR MIN HEIGHT TO A SAFE LEVEL FOR PUMP
-  // DO NOT SET TO LONGER THAN IT WOULD TAKE TO PUMP THE SYSTEM DRY AT LOWEST WATER LEVEL TRIGGER
+	delay(800);
+	adjustLevel();
+	Serial.print(avgDistance);
+	Serial.println(" is the average distance before sensorData()");
+	avgDistance = sensorData();
+	refreshDisplay();                                  // useful if debugging sensorData since it loops back around very quickly when not running the pump
+	enableAlarm(checkSensor, checkRelay, checkWater);  // checks all three global booleans that are modified by above functions and checks based on external sensor input only
+	watchdog.reset();
+	autoRunPump(8000);  // pass in the length of time you want to run pump in milliseconds, auto refers to self check in function for conditions
+	                    // IMPORTANT INTERGER FOR PUMP TIME, NOT CURRENTLY VARIABLE WHILE INSTALLED, TIME SHOULD EQUAL NO MORE THAN THE TIME TO PUMP FROM MAX OR MIN HEIGHT TO A SAFE LEVEL FOR PUMP
+	                    // DO NOT SET TO LONGER THAN IT WOULD TAKE TO PUMP THE SYSTEM DRY AT LOWEST WATER LEVEL TRIGGER
 }
